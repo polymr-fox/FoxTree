04d11f5216287cc6a27daeaea56b5fe5ee337a7e
Loginov_Arkadiy <loginov3.aa@edu.spbstu.ru>
2019-05-26 00:02:57 +0300

    add working homefragment

--- app/src/main/java/com/develop/loginov/fullstack/controller/activity/MainActivity.java
+++ app/src/main/java/com/develop/loginov/fullstack/controller/activity/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.develop.loginov.fullstack.R;
 import com.develop.loginov.fullstack.controller.fragment.ArticleFragment;
@@ -17,38 +19,41 @@ import com.develop.loginov.fullstack.model.Course;
 import static com.develop.loginov.fullstack.model.helper.FragmentHelper.*;
 
 public class MainActivity extends AppCompatActivity implements CourseFragment.OnCourseClickListener {
+    private View collapseLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final BottomNavigationView navView = findViewById(R.id.activity_main__nav_view);
+        collapseLayout = findViewById(R.id.activity_main__collapse_layout);
+        collapseLayout.setVisibility(View.GONE);
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
         changeFragment(new CourseFragment(), getSupportFragmentManager(), R.id.activity_main__container);
         navView.setSelectedItemId(R.id.navigation_courses);
     }
 
-    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    startActivity(new Intent(MainActivity.this, HomeActivity.class));
-//                    changeFragment(HomeFragment.newInstance(), getSupportFragmentManager(), R.id.activity_main__container);
-                    return true;
-                case R.id.navigation_events:
-                    changeFragment(new EventFragment(), getSupportFragmentManager(), R.id.activity_main__container);
-                    return true;
-                case R.id.navigation_courses:
-                    changeFragment(new CourseFragment(), getSupportFragmentManager(), R.id.activity_main__container);
-                    return true;
-                case R.id.navigation_articles:
-                    changeFragment(new ArticleFragment(), getSupportFragmentManager(), R.id.activity_main__container);
-                    return true;
-            }
-            return false;
+    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener = item -> {
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                changeFragment(HomeFragment.newInstance(), getSupportFragmentManager(), R.id.activity_main__container);
+                collapseLayout.setVisibility(View.VISIBLE);
+                return true;
+            case R.id.navigation_events:
+                changeFragment(new EventFragment(), getSupportFragmentManager(), R.id.activity_main__container);
+                collapseLayout.setVisibility(View.GONE);
+                return true;
+            case R.id.navigation_courses:
+                changeFragment(new CourseFragment(), getSupportFragmentManager(), R.id.activity_main__container);
+                collapseLayout.setVisibility(View.GONE);
+                return true;
+            case R.id.navigation_articles:
+                changeFragment(new ArticleFragment(), getSupportFragmentManager(), R.id.activity_main__container);
+                collapseLayout.setVisibility(View.GONE);
+                return true;
         }
+
+        return false;
     };
 
 
