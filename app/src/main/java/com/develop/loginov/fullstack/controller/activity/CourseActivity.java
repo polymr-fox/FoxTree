392023a002ee3e26db4520a6d1281b6efe247524
Loginov_Arkadiy <loginov3.aa@edu.spbstu.ru>
2019-05-25 18:08:56 +0300

    add list records

--- app/src/main/java/com/develop/loginov/fullstack/controller/activity/CourseActivity.java
+++ app/src/main/java/com/develop/loginov/fullstack/controller/activity/CourseActivity.java
@@ -2,9 +2,11 @@ package com.develop.loginov.fullstack.controller.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import com.develop.loginov.fullstack.R;
+import com.develop.loginov.fullstack.adapter.RecordAdapter;
 import com.develop.loginov.fullstack.model.Record;
 
 import java.util.Arrays;
@@ -13,21 +15,21 @@ import java.util.List;
 public class CourseActivity extends AppCompatActivity {
 
     private List<Record> records;
+    private RecyclerView.Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course);
         final RecyclerView recyclerView = findViewById(R.id.activity_course__recycler);
+
         records = getRecords();
+        adapter = new RecordAdapter(records);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
     }
 
     private List<Record> getRecords() {
-        return Arrays.asList(Record.of("IOS Разработка", "cl;msalmcl;samc;lamlcewfjfjefiwefjiwefjwe"),
-                             Record.of("IOS Разработка", "cl;msalmcl;samc;lamlcewfjfjefiwefjiwefjwe"),
-                             Record.of("IOS Разработка", "clmkekcmekmkekmceklamlcewfjfjefiwjiwefjwe"),
-                             Record.of("IOS Разработка", "cl;msalmcl;samc;lamlcewfjfjefiwefjiwefjwe"),
-                             Record.of("IOS Разработка", "cl;msalmcl;samc;lamlcewfjfjefiwefjiwefjwe"),
-                             Record.of("IOS Разработка", "cl;msalmcl;samc;lamlcewfjfjefiwefjiwefjwe"));
+        return Arrays.asList(Record.of("IOS Разработка", "cl;msalmcl;samc;lamlcewfjfjefiwefjiwefjwe"), Record.of("IOS Разработка", "cl;msalmcl;samc;lamlcewfjfjefiwefjiwefjwe"), Record.of("IOS Разработка", "clmkekcmekmkekmceklamlcewfjfjefiwjiwefjwe"), Record.of("IOS Разработка", "cl;msalmcl;samc;lamlcewfjfjefiwefjiwefjwe"), Record.of("IOS Разработка", "cl;msalmcl;samc;lamlcewfjfjefiwefjiwefjwe"), Record.of("IOS Разработка", "cl;msalmcl;samc;lamlcewfjfjefiwefjiwefjwe"));
     }
 }
