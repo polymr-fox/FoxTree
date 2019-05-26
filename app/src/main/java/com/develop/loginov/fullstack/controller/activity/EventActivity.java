90f5516aa0ac5cf80bc4e9ad4574a6c06ecaba22
Loginov_Arkadiy <loginov3.aa@edu.spbstu.ru>
2019-05-26 14:29:10 +0300

    event activity

--- app/src/main/java/com/develop/loginov/fullstack/controller/activity/EventActivity.java
+++ app/src/main/java/com/develop/loginov/fullstack/controller/activity/EventActivity.java
@@ -2,14 +2,19 @@ package com.develop.loginov.fullstack.controller.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.develop.loginov.fullstack.R;
 
-public class EventActivity extends AppCompatActivity {
-
+public class EventActivity extends CourseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_event);
+        final ImageView lockView = findViewById(R.id.activity_course__lock);
+        lockView.setVisibility(View.GONE);
+        final TextView textMembers = findViewById(R.id.activity_course__members);
+        textMembers.setText(R.string.sample_members_count_event);
     }
 }
