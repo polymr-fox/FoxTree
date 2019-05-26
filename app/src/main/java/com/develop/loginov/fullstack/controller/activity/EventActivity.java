b5d932301447e91ffeacc8ca56a1fe7cdeca4e8a
Loginov_Arkadiy <loginov3.aa@edu.spbstu.ru>
2019-05-26 15:32:54 +0300

    done article screen

--- app/src/main/java/com/develop/loginov/fullstack/controller/activity/EventActivity.java
+++ app/src/main/java/com/develop/loginov/fullstack/controller/activity/EventActivity.java
@@ -2,11 +2,14 @@ package com.develop.loginov.fullstack.controller.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.develop.loginov.fullstack.R;
+import com.develop.loginov.fullstack.dialog.CourseDescriptionDialog;
 
 public class EventActivity extends CourseActivity {
     @Override
@@ -16,5 +19,20 @@ public class EventActivity extends CourseActivity {
         lockView.setVisibility(View.GONE);
         final TextView textMembers = findViewById(R.id.activity_course__members);
         textMembers.setText(R.string.sample_members_count_event);
+    }
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.course_setting_edit:
+                return true;
+            case R.id.course_setting_description:
+                final CourseDescriptionDialog dialog = CourseDescriptionDialog.newInstance();
+                dialog.show(getSupportFragmentManager(), "Course description");
+                return true;
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return false;
     }
 }
